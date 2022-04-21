830bad85bac33458c59699b68dc917f0a0ca92fd
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-04-22 02:43:02 +0600

    Added Steps

---
+++ _steps.js
@@ -0,0 +1,29 @@
+/* 
+Basic SetUp
+
+ 1. Create a folder
+ 2. Open command line to that folder
+ 3. npm init
+ 4. npm install express
+ 5. Create index.js
+ ------------------------
+ For Express/Node Server
+ ------------------------
+ 1. require express
+ 2. create app variable
+ 3. declare port
+ 4. set app.get('/')
+ 5. listen to port
+ 6. node index.js 
+ 7. check your browser for that port
+
+ ----------
+ get
+ ----------
+ 1. cors middleware
+
+ -------------
+ Post
+ -------------
+ 1. app.use(express.json())
+*/
